93f8550efc85c4b5282f40e693c4f0ca0835acf0
Gonzalo Gras Cantou <guusygonzalo@gmail.com>
2019-09-19 21:04:01 -0300

    Los atributos que no estan marcados persistibles, no se guardan en la base de datos

--- ruby/spec/save_spec.rb
+++ ruby/spec/save_spec.rb
@@ -5,22 +5,32 @@ describe 'save' do
       class Persona
         include Orm
         has_one String, named: :nombre
+        attr_accessor :valor_no_persistido
       end
       @persona = Persona.new
       @persona.nombre = "Gonzalo gras cantou"
       @persona.save!
+      @persona_db = get_find_by_id(@persona.id)
     end
 
     it 'Le genera agrega la propiedad Id' do
       expect(@persona).to respond_to(:id)
     end
 
-    it 'Agrega el objeto a la base de datos' do
-      persona_DB = TADB::DB.table('persona')
-                      .entries
-                      .select { |entry| entry[:id] === @persona.id }
-                      .first
-      expect(persona_DB[:nombre]).to eq @persona.nombre
+    it 'Agrega el objeto a la DB con los datos marcados como persistibles' do
+      expect(@persona_db[:nombre]).to eq @persona.nombre
     end
+
+    it 'no agrega los atributos a la DB que no fueron marcados como persistibles' do
+      expect(@persona_db[:valor_no_persistido]).to eq nil
+    end
+
+    def get_find_by_id(id)
+      return TADB::DB.table('persona')
+                 .entries
+                 .select { |entry| entry[:id] === id }
+                 .first
+    end
+
   end
 end
