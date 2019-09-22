c912d9f814e4ed3424530fb2f0e812f3a75cd59c
Gonzalo Gras Cantou <guusygonzalo@gmail.com>
2019-09-22 20:30:57 -0300

    Agrego tests de dependencias compuestas del .refresh

--- ruby/spec/refresh_spec.rb
+++ ruby/spec/refresh_spec.rb
@@ -38,30 +38,58 @@ describe 'refresh' do
   end
 
   context 'cuando se se modifica un atributo persistido de un objeto que ya fue persistido' do
-    before do
-      class Casa
-        include Orm
-        has_one String, named: :color
+    context 'con dependencia simple' do
+      before do
+        class Casa
+          include Orm
+          has_one String, named: :color
+        end
+        class Patito
+          has_one Casa, named: :casa
+        end
+        @patito_persistido = Patito.new
+        @casa = Casa.new
+        @casa.color = "verde oscuro"
+        @casa.save!
+        @patito_persistido.casa = @casa
+        @patito_persistido.save!
+        @patito_persistido.casa.color = 'salmon'
       end
-      class Patito
-        has_one Casa, named: :casa
+      it 'se actualizan la propiedades en el atributo' do
+        expect(@patito_persistido.casa.color).to eq('salmon')
       end
-      @patito_persistido = Patito.new
-      @casa = Casa.new
-      @casa.color = "verde oscuro"
-      @casa.save!
-      @patito_persistido.casa = @casa
-      @patito_persistido.save!
-      @patito_persistido.casa.color = 'salmon'
-    end
-    it 'se actualizan la propiedades en el atributo' do
-      expect(@patito_persistido.casa.color).to eq('salmon')
-    end
-    context "y se hace refresh sobre el" do
-      it 'se vuelve el atributo persitido a su estado inicial' do
-        @patito_persistido.resfresh!
-        expect(@patito_persistido.casa.color).to eq('verde oscuro')
+      context "y se hace refresh sobre el" do
+        it 'se vuelve el atributo persitido a su estado inicial' do
+          @patito_persistido.resfresh!
+          expect(@patito_persistido.casa.color).to eq('verde oscuro')
+        end
       end
     end
+
+    context 'con dependencia compuesta' do
+      before do
+        @acero = Material.new
+        @acero.peso = "200"
+        @pala = Herramienta.new
+        @pala.material = @acero
+        @pepe = Obrero.new
+        @pepe.nombre = "pepe"
+        @pepe.herramienta = @pala
+        @pepe.save!
+
+        @pepe.herramienta.material.peso = "2500"
+      end
+      it 'se actualizan la propiedades en el atributo' do
+        expect(@pepe.herramienta.material.peso).to eq('2500')
+      end
+
+      context "y se hace refresh sobre el" do
+        it 'se vuelve el atributo persitido a su estado inicial' do
+          @pepe.resfresh!
+          expect(@pepe.herramienta.material.peso).to eq('200')
+        end
+      end
+    end
+
   end
 end
