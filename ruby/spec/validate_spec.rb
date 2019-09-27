60efb3fc26a5081f8038db71ad6c57d916030fbe
Gonzalo Gras Cantou <guusygonzalo@gmail.com>
2019-09-27 16:43:23 -0300

    Cuando se intenta persistir un atributo complejo el cual tiene errores en sus atributos internos, falla con un mensaje de error ya que no matchean los tipos

--- ruby/spec/validate_spec.rb
+++ ruby/spec/validate_spec.rb
@@ -2,6 +2,7 @@ describe 'validate' do
   after do
     TADB::DB.clear_all
   end
+  # TODO :  preguntar que onda con los atributos nil  ?  deberia explotar todo ?
 
   context 'cuando se trata de persistir atributo que es String con un numero' do
     before do
@@ -25,8 +26,48 @@ describe 'validate' do
       @contador = Contador.new
       @contador.acumulador = "sadasdasda"
     end
-    it 'deberia fallar con "El [ATRIBUTO] no es un [TIPO_ESPERADO]! valor actual : [VALOR_ACTUAL]"' do
+    it 'falla con "El [ATRIBUTO] no es un [TIPO_ESPERADO]! valor actual : [VALOR_ACTUAL]"' do
       expect { @contador.save! }.to raise_error("El acumulador no es un Numeric! valor actual : sadasdasda")
     end
   end
+
+  context 'cuando se trata de persisitir un atributo que es complejo' do
+    before do
+      class Portero
+        include Orm
+        has_one Numeric, named: :edad
+      end
+      class Edificio
+        include Orm
+        has_one Portero, named: :portero
+      end
+    end
+
+    context 'con un primitivo' do
+      before do
+        @edificio = Edificio.new
+        @edificio.portero = "hola"
+      end
+
+      it 'falla con "El [ATRIBUTO] no es un [TIPO_ESPERADO]! valor actual : [VALOR_ACTUAL]"' do
+        expect { @edificio.save! }.to raise_error("El portero no es un Portero! valor actual : hola")
+      end
+    end
+
+    context 'el cual tiene en su interior un atributo erroneo' do
+      before do
+        @portero = Portero.new
+        @portero.edad = "teclado"
+        @edificio = Edificio.new
+        @edificio.portero = @portero
+      end
+
+      it 'falla con "El [ATRIBUTO] no es un [TIPO_ESPERADO]! valor actual : [VALOR_ACTUAL]"' do
+        expect { @edificio.save! }.to raise_error("El edad no es un Numeric! valor actual : teclado")
+      end
+    end
+  end
+
+
+
 end
