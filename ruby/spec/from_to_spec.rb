bd8e7eb13f9600f2de9cc3f121827bff86b58d6b
Gonzalo Gras Cantou <guusygonzalo@gmail.com>
2019-09-28 00:12:21 -0300

    Cuando se le declara la propiedad from+to y el atributo no cumple con el rango, falla

--- ruby/spec/from_to_spec.rb
+++ ruby/spec/from_to_spec.rb
@@ -31,13 +31,40 @@ describe 'from y to' do
         has_one Integer, named: :jugadores, to: 10
       end
     end
-    context 'y se trata de guardar un objeto con menos valor que el from' do
+    context 'y se trata de guardar un objeto con mas valor que to' do
       before do
         @paintball = Paintball.new
         @paintball.jugadores = 25
       end
       it 'falla "El atributo [ATRIBUTO] es mayor a [FROM], valor actual : [VALOR]"' do
         expect { @paintball.save! }.to raise_error("El atributo jugadores es mayor a 10, valor actual : 25")
+      end
+    end
+  end
+
+  context 'cuando se le declara from y to' do
+    before do
+      class Empresa
+        include Orm
+        has_one Integer, named: :empleados, from: 50, to: 100
+      end
+    end
+    context 'y se trata de guardar un objeto con menos valor que el from ' do
+      before do
+        @telecom = Empresa.new
+        @telecom.empleados = 20
+      end
+      it 'falla "El atributo [ATRIBUTO] es mayor a [FROM], valor actual : [VALOR]"' do
+        expect { @telecom.save! }.to raise_error("El atributo empleados es menor a 50, valor actual : 20")
+      end
+    end
+    context 'y se trata de guardar un objeto con mas valor que to' do
+      before do
+        @globant = Empresa.new
+        @globant.empleados = 1000
+      end
+      it 'falla "El atributo [ATRIBUTO] es mayor a [FROM], valor actual : [VALOR]"' do
+        expect { @globant.save! }.to raise_error("El atributo empleados es mayor a 100, valor actual : 1000")
       end
     end
   end
